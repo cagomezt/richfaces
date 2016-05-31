03dde39ab3214abfde38fb96fb0996339848581d
Jiri Stefek <jstefek@redhat.com>
2016-05-31 12:24:49 +0200

    page loader refactored with graphene interceptors

--- build/build-resources/src/main/java/org/richfaces/arquillian/browser/PageLoader.java
+++ build/build-resources/src/main/java/org/richfaces/arquillian/browser/PageLoader.java
@@ -1,19 +1,30 @@
 package org.richfaces.arquillian.browser;
 
 import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.jboss.arquillian.drone.spi.DroneInstanceEnhancer;
 import org.jboss.arquillian.drone.spi.InstanceOrCallableInstance;
-import org.jboss.arquillian.graphene.proxy.GrapheneProxyUtil;
+import org.jboss.arquillian.graphene.proxy.GrapheneProxy;
+import org.jboss.arquillian.graphene.proxy.GrapheneProxyInstance;
+import org.jboss.arquillian.graphene.proxy.Interceptor;
+import org.jboss.arquillian.graphene.proxy.InvocationContext;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Intercepts call of <code>WebDriver#get(url)</code> and tries to load the page up to 3 times, while catching TimeoutException.
+ * Uses Graphene interceptors, so this enhancer needs to be called after GrapheneEnhancer.
+ *
+ * @author <a href="mailto:jstefek@redhat.com">Jiri Stefek</a>
+ */
 public class PageLoader implements DroneInstanceEnhancer<WebDriver> {
+
+    private static final int DEFAULT_NUMBER_OF_REPEATS = 3;
+    private static final String GET_METHOD_NAME = "get";
+    private static final Logger LOG = Logger.getLogger(PageLoader.class.getName());
+    private static final Interceptor pageLoadInterceptor = new PageLoadInterceptor();
 
     @Override
     public boolean canEnhance(InstanceOrCallableInstance instance, Class<?> droneType, Class<? extends Annotation> qualifier) {
@@ -21,26 +32,21 @@ public class PageLoader implements DroneInstanceEnhancer<WebDriver> {
     }
 
     @Override
-    public WebDriver deenhance(WebDriver enhancedInstance, Class<? extends Annotation> qualifier) {
-        if (enhancedInstance instanceof PageLoader) {
-            return ((PageReloader) enhancedInstance).getOriginalInstance();
-        } else {
-            return enhancedInstance;
+    public WebDriver deenhance(WebDriver instance, Class<? extends Annotation> qualifier) {
+        if (GrapheneProxy.isProxyInstance(instance)) {
+            GrapheneProxyInstance proxy = (GrapheneProxyInstance) instance;
+            proxy.unregisterInterceptor(pageLoadInterceptor);
         }
+        return instance;
     }
 
     @Override
-    public WebDriver enhance(WebDriver instance, Class<? extends Annotation> qualifier) {
-        return (WebDriver) Proxy.newProxyInstance(getClass().getClassLoader(), getClasses(instance), new PageReloaderImpl(instance));
-
-    }
-
-    private Class<?>[] getClasses(WebDriver instance) {
-        // reuse Graphene utils
-        Class<?>[] interfaces = GrapheneProxyUtil.getInterfaces(instance.getClass());
-        Class<?>[] result = Arrays.copyOf(interfaces, interfaces.length + 1);
-        result[interfaces.length] = PageReloader.class;
-        return result;
+    public WebDriver enhance(final WebDriver instance, Class<? extends Annotation> qualifier) {
+        if (GrapheneProxy.isProxyInstance(instance)) {
+            GrapheneProxyInstance proxy = (GrapheneProxyInstance) instance;
+            proxy.registerInterceptor(pageLoadInterceptor);
+        }
+        return instance;
     }
 
     @Override
@@ -48,64 +54,33 @@ public class PageLoader implements DroneInstanceEnhancer<WebDriver> {
         return -101;// run after Graphene (-100)
     }
 
-    public interface PageReloader {
+    private static final class PageLoadInterceptor implements Interceptor {
 
-        public WebDriver getOriginalInstance();
-
-        public void tryToLoadPage(String url);
-    }
-
-    public static class PageReloaderImpl implements PageReloader, InvocationHandler {
-
-        private static final int DEFAULT_NUMBER_OF_REPEATS = 3;
-        private static final String GET_METHOD_NAME = "get";
-
-        private final WebDriver originalInstance;
-
-        public PageReloaderImpl(WebDriver originalInstance) {
-            this.originalInstance = originalInstance;
+        @Override
+        public int getPrecedence() {
+            return 0;
         }
 
         @Override
-        public WebDriver getOriginalInstance() {
-            return originalInstance;
-        }
-
-        /**
-         * Proxies the "get" method of WebDriver.
-         */
-        @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            try {
-                if (method.getName().equals(GET_METHOD_NAME)) {
-                    tryToLoadPage(String.valueOf(args[0]));
-                    return null;
-                } else {
-                    return method.invoke(getOriginalInstance(), args);
-                }
-            } catch (InvocationTargetException e) {
-                throw e.getCause();
+        public Object intercept(InvocationContext context) throws Throwable {
+            if (context.getMethod().getName().equals(GET_METHOD_NAME)) {
+                tryToLoadPage((WebDriver) context.getTarget(), String.valueOf(context.getArguments()[0]));
+                return null;
+            } else {
+                return context.invoke();
             }
         }
 
-        /**
-         * Tries to load the page repeatedly.
-         *
-         * @see https://issues.jboss.org/browse/RF-14292
-         *
-         * @param url url to be opened
-         */
-        @Override
-        public void tryToLoadPage(String url) {
+        private void tryToLoadPage(WebDriver webDriver, String url) {
             for (int i = 1; i <= DEFAULT_NUMBER_OF_REPEATS; i++) {
                 try {
-                    getOriginalInstance().get(url);
+                    webDriver.get(url);
                     return;
                 } catch (TimeoutException e) {
                     if (i == DEFAULT_NUMBER_OF_REPEATS) {
                         throw e;
                     }
-                    System.out.println("Page was not loaded withinn timeout. Trying to load it again, attempt #" + (i + 1));
+                    LOG.log(Level.INFO, "Page was not loaded within timeout. Trying to load it again, attempt #{0}", (i + 1));
                 }
             }
         }
